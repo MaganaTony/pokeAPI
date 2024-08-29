c88ed466f68791c296d9d8e36de5c8430639118b
Antonio Magana <antoniomagana@172-10-19-149.lightspeed.clmasc.sbcglobal.net>
2024-08-29 11:24:17 -0600

    Added capitalize to each data.name on the fetch for the useEffect

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -2,21 +2,27 @@ import { useEffect, useState } from "react"
 
 export default function Pokemon({ name }) {
     const [pokemon, setPokemon] = useState({})
+
+    function capitalizeFirstLetter(string){
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
     
     useEffect(() => {
         fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
             .then((response) => response.json())
             .then((data) => {
-                setPokemon(data)
+                data.name = capitalizeFirstLetter(data.name)
+                setPokemon(data)                
             })
             .catch(error => {
                 console.error('Error:', error)
             })
-    }, [])
-    
+    }, [])    
+
+
     return (
-        <article className="bg-zinc-700 border">
-            <h2>{pokemon.name}</h2>
+        <article className="bg-zinc-700 border flex flex-col items-center p-2">
+            <h2 className="font-bold bg-slate-900 w-full text-center" >{pokemon.name}</h2>
             <img className="size-14" src={pokemon.sprites?.other["official-artwork"]?.front_default} alt={pokemon.name} />
         </article>
     )
