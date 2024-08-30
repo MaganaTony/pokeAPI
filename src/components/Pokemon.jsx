fcca7f95e0b4b1f181731b83c30a9ff966f20aae
MaganaTony <maganajarm@gmail.com>
2024-08-29 19:23:15 -0600

    Pokedex styles hover

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -21,7 +21,9 @@ export default function Pokemon({ name }) {
 
     return (
         <article className="bg-zinc-700 border border-white/60 flex flex-col items-center p-2 rounded-lg gap-2 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl group">
-            <h2 className="font-bold bg-slate-900/80 w-full text-center rounded-lg my-2">{pokemon.name}</h2>
+            <h2 className="font-bold w-full text-center rounded-lg my-2 bg-slate-900/80 transition-colors duration-300 group-hover:bg-gradient-to-r group-hover:from-blue-500 group-hover:to-yellow-500">
+                {pokemon.name}
+            </h2>
             <img className="size-14 transform transition-transform duration-300 group-hover:scale-110" src={pokemon.sprites?.other["official-artwork"]?.front_default} alt={pokemon.name} />
         </article>
     );
