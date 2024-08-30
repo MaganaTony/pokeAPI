0efe67f8425fee2576ff5b30ac8373b2d0012a12
MaganaTony <maganajarm@gmail.com>
2024-08-29 19:12:59 -0600

    Pokedex styles hover

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,29 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export default function Pokemon({ name }) {
-    const [pokemon, setPokemon] = useState({})
+    const [pokemon, setPokemon] = useState({});
 
-    function capitalizeFirstLetter(string){
+    function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
-    
+
     useEffect(() => {
         fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
             .then((response) => response.json())
             .then((data) => {
-                data.name = capitalizeFirstLetter(data.name)
-                setPokemon(data)                
+                data.name = capitalizeFirstLetter(data.name);
+                setPokemon(data);
             })
-            .catch(error => {
-                console.error('Error:', error)
-            })
-    }, [])    
-
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }, []);
 
     return (
-        <article className="bg-zinc-700 border border-white/60 flex flex-col items-center p-2 rounded-lg gap-2">
-            <h2 className="font-bold bg-slate-900/80 w-full text-center rounded-lg my-2" >{pokemon.name}</h2>
-            <img className="size-14" src={pokemon.sprites?.other["official-artwork"]?.front_default} alt={pokemon.name} />
+        <article className="bg-zinc-700 border border-white/60 flex flex-col items-center p-2 rounded-lg gap-2 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+            <h2 className="font-bold bg-slate-900/80 w-full text-center rounded-lg my-2">{pokemon.name}</h2>
+            <img className="size-14 transition-transform duration-300 hover:scale-110" src={pokemon.sprites?.other["official-artwork"]?.front_default} alt={pokemon.name} />
         </article>
-    )
+    );
 }
